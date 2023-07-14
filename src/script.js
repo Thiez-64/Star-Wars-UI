995eb11e91ad125aefaa0619a9b0cd58813a15a5
Thiez-64 <79849782+Thiez-64@users.noreply.github.com>
2023-07-14 09:13:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/script.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var clickCount = 0;
+  var dotIndex = 1;
+  var lineIndex = 0;
+
+  var logos = document.getElementById("logos");
+  var logosWidth = 80;
+
+  var titles = document.getElementById("titles");
+  var titlesWidth = 509;
+
+  var texts = document.getElementById("texts");
+  var textsWidth = 420;
+
+  var dates = document.getElementById("dates");
+  var datesWidth = 532;
+
+  var posters = document.getElementById("posters");
+  var postersWidth = 430;
+
+  var bar = document.getElementById("bar");
+
+  document.getElementById("arrow-right").addEventListener("click", function () {
+    clickCount++;
+
+    var logoDistance = clickCount * logosWidth;
+    var posterDistance = clickCount * postersWidth;
+    var titleDistance = clickCount * titlesWidth;
+    var textDistance = clickCount * textsWidth;
+    var dateDistance = clickCount * datesWidth;
+
+    var dot = document.getElementById("dot" + (dotIndex + 1));
+    var line = document.getElementById("line_progress" + (lineIndex + 1));
+
+    logos.style.transform = `translateX(-${logoDistance}px)`;
+    posters.style.transform = `translateX(-${posterDistance}px)`;
+    titles.style.transform = `translateX(-${titleDistance}px)`;
+    texts.style.transform = `translateX(-${textDistance}px)`;
+    dates.style.transform = `translateX(-${dateDistance}px)`;
+
+    dot.style.backgroundColor = `#fff38a`;
+
+    line.classList.add("line_progress_selected");
+
+    dotIndex = (dotIndex + 1) % 6 === 0 ? 6 : (dotIndex + 1) % 6;
+    lineIndex = (lineIndex + 1) % 6 === 0 ? 6 : (lineIndex + 1) % 6;
+
+    bar.classList.add("bar_selected");
+  });
+
+  document.getElementById("arrow-left").addEventListener("click", function () {
+    clickCount--;
+
+    var logoDistance = clickCount * logosWidth;
+    var posterDistance = clickCount * postersWidth;
+    var titleDistance = clickCount * titlesWidth;
+    var textDistance = clickCount * textsWidth;
+    var dateDistance = clickCount * datesWidth;
+
+    dot = document.getElementById("dot" + dotIndex);
+    line = document.getElementById("line_progress" + lineIndex);
+
+    logos.style.transform = `translateX(-${logoDistance}px)`;
+    posters.style.transform = `translateX(-${posterDistance}px)`;
+    titles.style.transform = `translateX(-${titleDistance}px)`;
+    texts.style.transform = `translateX(-${textDistance}px)`;
+    dates.style.transform = `translateX(-${dateDistance}px)`;
+
+    dot.style.backgroundColor = `#827b68`;
+    // line.style.backgroundColor = `#827b68`;
+
+    line.classList.remove("line_progress_selected");
+
+    dotIndex = dotIndex - 1;
+    lineIndex = lineIndex - 1;
+  });
+});
